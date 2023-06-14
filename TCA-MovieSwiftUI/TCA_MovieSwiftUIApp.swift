47b9c5808b2371d30735d4c01ef8d44f59676556
Hitesh Savaliya <hitesh.savaliya@gmail.com>
2023-06-14 01:18:51 +0100

    Initial Commit

---
+++ TCA-MovieSwiftUI/TCA_MovieSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_MovieSwiftUIApp.swift
+//  TCA-MovieSwiftUI
+//
+//  Created by Hitesh Savaliya on 14/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TCA_MovieSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
